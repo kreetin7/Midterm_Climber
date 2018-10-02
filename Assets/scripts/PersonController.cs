fe9af7dd513a7d03f27756334be7eb4ae856b971
Ren H <kerensahughes@gmail.com>
2018-10-01 22:32:43 -0400

    Prototype 1 commit: mechanics

---
+++ Assets/scripts/PersonController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//put on capsule player
+//first person controller of player model, handels movement and looking
+public class PersonController : MonoBehaviour
+{
+	private Rigidbody rBody;
+
+	//public float gravity = 40f; 
+
+	public float moveSpeed = 20f;
+	public float lookSpeed = 100f;
+	public float jumpSpeed = 0.5f;
+
+	private Vector3 inputVector;
+
+	//public Transform Groundcheck;
+	//public float groundcheckRadius;
+	//public LayerMask whatisGrounded;
+	//private bool Grounded;
+
+
+	//private Vector3 MoveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start()
+	{
+		rBody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//camera
+		float mouseX = Input.GetAxis("Mouse X") * lookSpeed * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * lookSpeed * Time.deltaTime;
+
+		transform.Rotate(0f, -mouseX, 0f);
+		Camera.main.transform.localEulerAngles += new Vector3(mouseY, 0f, 0f);
+		Camera.main.transform.localEulerAngles -= new Vector3(0f, 0f, Camera.main.transform.localEulerAngles.z);
+
+		//movement
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		inputVector = transform.forward * vertical * moveSpeed;
+		inputVector += transform.right * horizontal * moveSpeed;
+
+		//jump? 
+
+		float up = Input.GetAxis("Jump");
+
+		inputVector -= transform.up * up * jumpSpeed;
+
+		//gravity
+		//MoveDirection.y = gravity * Time.deltaTime; 
+
+
+
+
+
+
+
+
+	}
+
+	void FixedUpdate()
+	{
+		rBody.velocity = inputVector;
+		//grounded
+		/*Grounded = Physics2D.OverlapCircle(Groundcheck.position, groundcheckRadius, whatisGrounded);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			//no double-jump
+			if (Grounded)
+			{
+				rBody.velocity = new Vector2(rBody.velocity.x, jumpSpeed);
+			}
+		}*/
+	}
+}
---
+++ Assets/scripts/TextScript.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+
+	public int progress = 0;
+	public Text hint;
+
+	public Light CoffeeLight;
+	public Light MicrowaveLight;
+	public Light ComputerLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (progress == 0 && (CoffeeLight.enabled == false))
+		{
+			hint.text = "Ugh, another Monday morning. ";
+			hint.text += "Better get the coffee going."; 
+		}
+
+		if (CoffeeLight.enabled == true)
+		{
+			progress = 1; 
+		}
+
+		if (progress == 1 && (MicrowaveLight.enabled == false))
+		{
+			hint.text = "Great, coffee's going. ";
+			hint.text += "Now for the oatmeal, find the microwave."; 
+		}
+
+		if (MicrowaveLight.enabled == true)
+		{
+			progress = 2; 
+		}
+
+		if (progress == 2 && (ComputerLight.enabled == false))
+		{
+			hint.text = "Oatmeal's going! Crap did I save my paper? Better go do that."; 
+		}
+
+		if (ComputerLight.enabled == true)
+		{
+			progress = 3; 
+		}
+
+		if (progress == 3)
+		{
+			hint.text = "Whew, crisis averted. Oh wait my books are still up there, better grab those."; 
+		}
+		
+	}
+}
